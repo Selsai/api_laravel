170914c7110df550f9894048f1935cf8d42067a4
Amairi <selsabilamairi@gmail.com>
2026-02-10 16:28:05 +0100

    Activité 5

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,12 @@ Route::middleware('throttle:10,1')->group(function () {
 Route::middleware('auth:sanctum')->post('/logout', [UserController::class, 'logout']);
 
 // Routes publiques pour les livres (lecture seule)
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{book}', [BookController::class, 'show']);
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
 
 // Routes protégées (création, modification, suppression)
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/books', [BookController::class, 'store']);
-    Route::match(['patch', 'put'], '/books/{book}', [BookController::class, 'update']);
-    Route::delete('/books/{book}', [BookController::class, 'destroy']);
+    Route::post('/books', [BookController::class, 'store'])->name('books.store');
+    Route::match(['patch', 'put'], '/books/{book}', [BookController::class, 'update'])->name('books.update');
+    Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
 });
